053e3d89f7b7eea2824c13159b82c2dedf2a76a3
Elias <lollerrmns@gmail.com>
2023-10-18 20:10:12 -0300

    Front end e ajustes de server

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { IMC, ValueException } = require('healthcalc-node-package');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.static('public')); // Servir arquivos estáticos a partir do diretório 'public'
+
+// Rota para calcular o IMC
+app.get('/calcular-imc', (req, res) => {
+  try {
+    const peso = parseFloat(req.query.peso);
+    const altura = parseFloat(req.query.altura);
+
+    if (isNaN(peso) || isNaN(altura) || altura <= 0) {
+      throw new ValueException('Valores inválidos');
+    }
+
+    const imcCalculator = new IMC();
+    const imc = imcCalculator.calcularIMC(peso, altura);
+    const classificacao = imcCalculator.retornarClassificacaoIMC(imc);
+
+    res.json({
+      imc: imc,
+      classificacao: classificacao,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
